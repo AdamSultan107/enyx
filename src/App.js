bffb85692631a7b3b1bac5ec8333cdec73687a74
Adam Sultan <adamsult@andrew.cmu.edu>
2025-03-08 04:57:03 -0500

    Main Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Foot from './components/Footer'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+      <Foot />
+    </>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,57 +1,58 @@
-"use client";
+import React from "react";
+import { BsFacebook, BsTwitter, BsLinkedin } from "react-icons/bs";
+import enyxLogo from "../enyxwhitelogo.png";
+import "../styles/footer.css";
 
-import { Footer } from "flowbite-react";
-import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
-
-export function Component() {
+const Footer = () => {
   return (
-    <Footer container>
-      <div className="w-full">
-        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-          <div>
-            <Footer.Brand
-              href="https://flowbite.com"
-              src="https://flowbite.com/docs/images/logo.svg"
-              alt="Flowbite Logo"
-              name="Flowbite"
-            />
+    <footer className="footer">
+      <div className="footer-container">
+        {/* Brand Section */}
+        <div className="footer-brand">
+          <a href="/">
+            <img src={enyxLogo} alt="Enyx Logo" className="footer-logo" />
+          </a>
+        </div>
+
+        {/* Footer Links */}
+        <div className="footer-links">
+          <div className="footer-section">
+            <h4>About</h4>
+            <ul>
+              <li><a href="#">Company</a></li>
+              <li><a href="#">Mission</a></li>
+            </ul>
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-            <div>
-              <Footer.Title title="about" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Flowbite</Footer.Link>
-                <Footer.Link href="#">Tailwind CSS</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="Follow us" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Github</Footer.Link>
-                <Footer.Link href="#">Discord</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="Legal" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
+          <div className="footer-section">
+            <h4>Follow Us</h4>
+            <ul>
+              <li><a href="#">Github</a></li>
+              <li><a href="#">Discord</a></li>
+            </ul>
+          </div>
+          <div className="footer-section">
+            <h4>Legal</h4>
+            <ul>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Terms & Conditions</a></li>
+            </ul>
           </div>
         </div>
-        <Footer.Divider />
-        <div className="w-full sm:flex sm:items-center sm:justify-between">
-          <Footer.Copyright href="#" by="Flowbite™" year={2022} />
-          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-            <Footer.Icon href="#" icon={BsFacebook} />
-            <Footer.Icon href="#" icon={BsInstagram} />
-            <Footer.Icon href="#" icon={BsTwitter} />
-            <Footer.Icon href="#" icon={BsGithub} />
-            <Footer.Icon href="#" icon={BsDribbble} />
-          </div>
+
+        {/* Social Media Icons */}
+        <div className="footer-social">
+          <a href="#"><BsFacebook /></a>
+          <a href="#"><BsLinkedin /></a>
+          <a href="#"><BsTwitter /></a>
         </div>
       </div>
-    </Footer>
+
+      {/* Footer Bottom Section */}
+      <div className="footer-bottom">
+        <p>© {new Date().getFullYear()} Enyx Therapeutics. All rights reserved.</p>
+      </div>
+    </footer>
   );
-}
+};
+
+export default Footer;
