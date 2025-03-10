cf463edf76cb7491abc7fdcf1bdfb485bcddb54a
Adam Sultan <adamsult@andrew.cmu.edu>
2025-03-10 12:33:49 -0400

    Redownloaded Chakra, threw away Hero CSS file, added Hero to Homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider } from "@chakra-ui/react"
 import Navbar from './components/Navbar'
 import Foot from './components/Footer'
 import Hero from './components/Hero'
 
 function App() {
   return (
-    <>
+    <ChakraProvider>
       <BrowserRouter>
         <Navbar />
         <Hero />
         <Foot />
       </BrowserRouter>
-    </>
+    </ChakraProvider>
   );
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,38 @@
-import React from "react";
-import HeroImage from "../assets/heroimage.png";
-
+import { Box, Button, Container, Flex, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import dnaImage from "../assets/heroimage.png"; // Ensure image is in the assets folder
 
 const Hero = () => {
-    return (
-        <div className="hero">
-            <div className="hero-container">
-                <div className="hero-text">
-                    <h1>Enyx Therapeutics</h1>
-                    <p>Revolutionizing the future of medicine.</p>
-                </div>
-                <div className="hero-image">
-                    <img src={HeroImage} alt="Hero Image" />
-                </div>
-            </div>
-        </div>
-    );
-    };
+  return (
+    <Box bg="white" py={20}>
+      <Container maxW="7xl">
+        <Flex
+          align="center"
+          justify="space-between"
+          direction={{ base: "column", md: "row" }}
+          gap={10}
+        >
+          {/* Left Side - Text Content */}
+          <Stack spacing={6} maxW={{ base: "100%", md: "50%" }} textAlign={{ base: "center", md: "left" }}>
+            <Heading as="h1" size="2xl" fontWeight="bold">
+              Transforming Lives <br /> Through Gene Therapy
+            </Heading>
+            <Text fontSize="lg" color="gray.600">
+              Pioneering cutting-edge genetic treatments to cure rare and life-threatening diseases.
+            </Text>
+            <Box w="100px" h="4px" bg="blue.500"></Box>
+            <Button colorScheme="blackAlpha" bg="black" size="lg" _hover={{ bg: "gray.700" }}>
+              Learn More
+            </Button>
+          </Stack>
+
+          {/* Right Side - Image */}
+          <Box maxW={{ base: "100%", md: "50%" }}>
+            <Image src={dnaImage} alt="DNA Structure" borderRadius="md" />
+          </Box>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
 
 export default Hero;
